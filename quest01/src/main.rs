46da26ccd373a485bcefe828f144ed6a0782ef52
KatoMono Enkeli <k4t0mono@gmail.com>
2017-11-11 11:19:07 -0200

    Basic I/O

--- quest01/src/main.rs
+++ quest01/src/main.rs
@@ -1,3 +1,12 @@
+se std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read_line");
+
+    println!("You guessed: {}", guess);
 }
